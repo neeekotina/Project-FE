1dc6e998ff34891666392293905adc0f041aafa1
neeekotina <0000110328065xsp@al.educacao.sp.gov.br>
2025-06-12 15:55:32 -0300

    Update digianscript.js

--- landpage/landpage/digianscript.js
+++ landpage/landpage/digianscript.js
@@ -1,14 +1,30 @@
 function irparaSobre() {
-    const imagem = document.getElementById("isobre")
+    const imagem = document.getElementById("isobre");
+    const posicaoY = imagem.getBoundingClientRect().top;
+    window.scrollTo({
+        top: posicaoY,
+        behavior: "smooth"
+    });   
+}
+function irparaServ() {
+    const imagem = document.getElementById("iserv");
+    const posicaoY = imagem.getBoundingClientRect().top;
+    window.scrollTo( {
+        top: posicaoY,
+        behavior: "smooth"
+    });
+
+}
+function irparaCont() {
+    const imagem = document.getElementById("icont");
     const posicaoY = imagem.getBoundingClientRect().top;
     window.scrollTo({
         top: posicaoY,
         behavior: "smooth"
     });
-
-    }
-function irparCont() {
-    const imagem = document.getElementById("iserv")
+}
+function irparaInicio() {
+    const imagem = document.getElementById("ini");
     const posicaoY = imagem.getBoundingClientRect().top;
     window.scrollTo({
         top: posicaoY,
